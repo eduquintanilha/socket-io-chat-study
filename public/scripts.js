f1decfcfd3d565ef6c9810a12e40f5a19d56bee0
Eduardo Quintanilha <quintanilhaedu@gmail.com>
2021-10-04 17:06:16 +0000

    Feat: Now, the chat log is saved on DB.

--- public/scripts.js
+++ public/scripts.js
@@ -1,25 +1,78 @@
-    var socket = io('https://3000-aae43d19-ce65-4d50-ad5d-886b104691e2.ws-us02.gitpod.io');
-
-   
-    
-    socket.on('connect', () => {
-        console.log(socket.id);
-    });
+var socket = io();
 
 
 
-    $('#chat').submit(function(event) {
-        event.preventDefault();
+socket.on('connect', () => {
+    console.log(socket.id);
 
-        var author = $('input[name=username]').val();
-        var message = $('input[name=message]').val();
+});
 
-        if(author.length && message.length) {
-            var messageObject = {
-                author: author,
-                message: message,
-            };
 
-            socket.emit('sendMessage', messageObject);
-        }
-    })
+socket.on('chatHistory', (history) => {
+    console.log(history);
+
+    var chatBox = document.querySelector('.messages');
+    chatBox.value = '';
+
+    if (history) {        
+            var newMessage = document.createElement('p');
+            newMessage.innerHTML = `<b>${history.author}</b> [${date()}]: ${history.message}`;
+
+            console.warn(newMessage);
+            chatBox.appendChild(newMessage);
+        }    
+});
+
+var username = localStorage.getItem("username");
+if (username) {
+    document.querySelector('#username').setAttribute('style', 'display:none');
+}
+var chatHistory = null;
+
+function date() {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    return dateTime;
+}
+
+$('#chat').submit(function(event) {
+    event.preventDefault();
+
+    document.querySelector('#username').setAttribute('style', 'display:none')
+
+
+    var author = $('input[name=username]').val();
+    var message = $('input[name=message]').val();
+
+    if (username) {
+        author = username;
+    }
+
+    localStorage.setItem("username", author);
+
+    if ((author.length || username.length) && message.length) {
+        var messageObject = {
+            author: author,
+            message: message,
+        };
+
+        socket.emit('sendMessage', messageObject);
+
+        document.querySelector('#text').value = '';
+
+
+    }
+})
+
+socket.on('sendMessage', (msg) => {
+    var chatBox = document.querySelector('.messages');
+    var newMessage = document.createElement('p');
+    newMessage.innerHTML = `<b>${msg.author}</b> [${date()}]: ${msg.message}`;
+
+    chatHistory += newMessage.innerHTML;
+
+    chatBox.appendChild(newMessage);
+
+})
--- server.js
+++ server.js
@@ -1,26 +1,67 @@
+const Database = require("@replit/database")
 const express = require('express');
 const path = require('path');
 
+const db = new Database();
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine','html');
+app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
     res.render('index.html');
 });
 
+//var chatHistory = [];
+
 io.on('connection', socket => {
+    
     console.log(`Socket connected | Client_ID => ${socket.id}`);
 
+    //Sending chat chatHistory
+    /*db.get("chatHistory").then(value => {
+        console.log(value);
+        io.emit('chatHistory', value);
+    });
+    */
+    
+    // Get all db keys starting with match string
+    db.list("chatHistory").then(matches => {       
+        matches.forEach((key) => {
+            // Deleting all keys
+            //db.delete(key).then(() => {});
+
+            db.get(key).then(value => {
+                console.log(value);
+                // Send chat key to frontend
+                io.emit('chatHistory', value)
+            })
+        })           
+    });
+        
+
+
     socket.on('sendMessage', data => {
+        console.log('Message received on server')
         console.log(data);
+        
+        // Add message to chat chatHistory DB
+        db.set(`chatHistory_${Date.now()}`, data)
+            .then(() => {
+                console.log(`Added message to chatHistory DB`);
+                console.log(data);
+        })
+    
+        // Sending message to frontend
+        io.emit('sendMessage', data);
     })
 })
 
 
+
+console.log('Server listening on port: 3000')
 server.listen(3000);
